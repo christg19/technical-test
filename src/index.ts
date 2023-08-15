8a85f71c5e37a58f139c42ecb181d1f83472b15f
christgb <srsteve1913@gmail.com>
2023-08-15 17:58:20 -0400

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata"
+import app from './app'
+import { AppDataSource } from './db'
+
+async function main(){
+    try {
+        AppDataSource.initialize()
+        console.log('Database Connected')
+        app.listen(3000)
+        console.log('Server is listening on port', 3000)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main()
+
+
+
